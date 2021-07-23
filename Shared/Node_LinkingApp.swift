9fdc7ee065e88292acb484b9be9a17893482515c
Beau Nouvelle <beau@getswifty.dev>
2021-07-23 22:10:24 +1000

    Initial Commit

---
+++ Shared/Node_LinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Node_LinkingApp.swift
+//  Shared
+//
+//  Created by Beau Nouvelle on 23/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Node_LinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
